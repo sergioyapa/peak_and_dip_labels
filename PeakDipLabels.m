f4f4ea733933b58035e2715eb88b9a7a2374f9d2
Sergio Yanez-Pagans <syapa@email.arizona.edu>
2021-03-28 00:22:18 -0700

    Add files via upload

---
+++ PeakDipLabels.m
@@ -0,0 +1,29 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%               Peak and dip labels 
+%            Sergio Yanez-Pagans 2021
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Find, plot, and label peaks
+
+function [pks,pks_locs,dip,dip_locs] = PeakDipLabels(x,y,hs,vs)
+
+[pks,pks_locs] = findpeaks(y,x); % find peaks and their locations
+[dip,dip_locs] = findpeaks(-y,x); % find dips and their locations
+figure('WindowState','maximized') % start a maximized figure window
+plot(x,y,'LineWidth',2,'Color','b'); % plot input arrays
+hold on;
+plot(pks_locs,pks,'ok','LineWidth',1.5,'MarkerEdgeColor','k','MarkerFaceColor','r','MarkerSize',10) % plot peaks in their locations
+plot(dip_locs,-dip,'ok','LineWidth',1.5,'MarkerEdgeColor','k','MarkerFaceColor','g','MarkerSize',10) % plot dips in their locations
+ylim([min(y)-0.5 max(y)+0.5]) % leave some y margin
+text(pks_locs+hs,pks+vs,num2str(pks_locs','%.1f'),'FontSize',16); % place dip labels
+text(dip_locs+hs,-dip-vs,num2str(dip_locs','%.1f'),'FontSize',16); % place dip labels
+box on
+% lines below control some plot aspects to give them a nice look
+set(gca, 'Layer', 'top')
+set(gca,'fontsize',28) 
+set(gca,'linewidth',3)
+set(gca,'FontName','Times New Roman')
+set(gcf, 'Position', [60,40,1150,600])
+set(gcf,'color','w'); %%%
+pbaspect([2 1 1])
+
+end
